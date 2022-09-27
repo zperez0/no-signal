81004c543e0a4c4d1dd09bc0e0685ddfe9735d47
Yazmin Perez <zoeperez333@gmail.com>
2022-09-27 16:21:29 -0700

    setup sidebar

---
+++ src/components/navbar/SideBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaBeer, FaPoo } from 'react-icons/fa';
+
+const SideBar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 flex flex-col
+                  bg-white dark:bg-gray-900 shadow-lg">
+                    
+        <SideBarIcon icon={<FaBeer size="24" />} />
+        
+        <SideBarIcon icon={<BsPlus size="32" />} />
+        <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+        <SideBarIcon icon={<FaPoo size="20" />} />
+        
+        <SideBarIcon icon={<BsGearFill size="22" />} />
+    </div>
+  );
+};
+const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
+  <div className="sidebar-icon group">
+    {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">
+      {text}
+    </span>
+  </div>
+);
+
+export default SideBar
