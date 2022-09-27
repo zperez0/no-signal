dae40b9f80f6bb4b712be9996acd0ee0e40e595a
Yazmin Perez <zoeperez333@gmail.com>
2022-09-26 21:17:53 -0700

    WIP: setup routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Header from "./../src/components/Header";
-import NoteControl from "./../src/components/NoteControl";
+// import NoteControl from "./../src/components/NoteControl";
+// import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./components/users/SignIn";
+import SignUp from "./components/users/SignUp";
+import Account from "./components/users/Account";
 
 function App() {
   return (
-    <React.Fragment>
-      <Header />
-      <NoteControl />
-    </React.Fragment>
+    <Router>
+    <Header />
+    <Routes>
+      {/* <Route path="/sign-in" element={<SignIn />} /> */}
+      <Route path="/" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/account" element={<Account />} />
+      {/* <Route path="/" element={<HomePage />} /> */}
+    </Routes>
+  </Router>
   );
 }
 
