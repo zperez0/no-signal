ba7d90025e27d8f7f8e4f43a9fcc9cf518095732
Yazmin Perez <zoeperez333@gmail.com>
2022-09-27 16:22:19 -0700

    add SideBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from "react";
-import Header from "./../src/components/Header";
-// import NoteControl from "./../src/components/NoteControl";
+import Header from "./components/Header";
+import NoteControl from "../src/components/notes/NoteControl";
 // import HomePage from "./components/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./components/users/SignIn";
 import SignUp from "./components/users/SignUp";
 import Account from "./components/users/Account";
+import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import SideBar from "./components/navbar/SideBar";
 
 function App() {
   return (
-    <Router>
+    <>
     <Header />
-    <Routes>
-      {/* <Route path="/sign-in" element={<SignIn />} /> */}
-      <Route path="/" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/account" element={<Account />} />
-      {/* <Route path="/" element={<HomePage />} /> */}
-    </Routes>
-  </Router>
+    <AuthContextProvider>
+    <Router>
+    <SideBar />
+      <Routes>
+        {/* <Route path="/sign-in" element={<SignIn />} /> */}
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/account" element={
+        <ProtectedRoute>
+          <Account />
+          </ProtectedRoute> } />
+          <Route path="/notes" element={<NoteControl />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+      </Routes>
+    </Router>
+    </AuthContextProvider>
+    </>
   );
 }
 
