14d481a5a740696d60e9a47f7e31d60e08022e55
Yazmin Perez <zoeperez333@gmail.com>
2022-09-28 21:27:03 -0700

    WIP: style note form

--- src/components/notes/NoteControl.jsx
+++ src/components/notes/NoteControl.jsx
@@ -95,12 +95,12 @@ const NoteControl = () => {
         onClickingEdit={handleEditClick}
       />
     );
-    buttonText = "Return Home";
+    buttonText = "Return to note list";
   } else if (formVisibleOnPage) {
     currentlyVisibleState = (
       <NewNoteForm onNewNoteCreation={handleAddingNewNoteToList} />
     );
-    buttonText = "Return Home";
+    buttonText = "Return to note list";
   } else {
     currentlyVisibleState = (
       <NoteList
@@ -113,7 +113,7 @@ const NoteControl = () => {
   return (
     <>
       {currentlyVisibleState}
-      <div className="mt-5">
+      <div className="button">
       {error ? null : <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" onClick={handleClick}>{buttonText}</button>}
       </div>
     </>
--- src/components/notes/NoteDetail.js
+++ src/components/notes/NoteDetail.js
@@ -1,16 +1,25 @@
 import React from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import Card from "./../UI/Card";
 
 function NoteDetail(props) {
   const { note, onClickingDelete, onClickingEdit } = props;
 
   return (
     <>
-      <h1>Note Detail</h1>
-      <h2>{note.title}</h2>
-      <h3>{note.body}</h3>
-      <button onClick={ onClickingEdit }>Update Note</button>
-      <button onClick={() => onClickingDelete(note.id) }>Delete Note</button>
+      <div className="detail">
+        <Card>
+          <h1>Note Detail</h1>
+          <h2>{note.title}</h2>
+          <h3>{note.body}</h3>
+          <div className="btn-detail">
+            <button onClick={onClickingEdit}>Edit</button>
+            <button onClick={() => onClickingDelete(note.id)}>
+              Delete
+            </button>
+          </div>
+        </Card>
+      </div>
     </>
   );
 }
@@ -18,7 +27,7 @@ function NoteDetail(props) {
 NoteDetail.propTypes = {
   note: PropTypes.object,
   onClickingDelete: PropTypes.func,
-  onClickingEdit: PropTypes.func
-}
+  onClickingEdit: PropTypes.func,
+};
 
 export default NoteDetail;
