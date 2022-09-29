a8e1851aed00269bb3868aff409a4dad85c088c7
Yazmin Perez <zoeperez333@gmail.com>
2022-09-28 18:57:31 -0700

    add card and style notes

--- src/components/notes/Note.jsx
+++ src/components/notes/Note.jsx
@@ -1,27 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {
-	Container,
-	Grid,
-	Paper,
-  Box
-} from "@mui/material";
+import Card from "../UI/Card";
+import "./NoteItem.css";
 
 function Note(props) {
   return (
     <React.Fragment>
-<div className="mt-12 h-20 flex flex-wrap justify-center">
-<div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-  <div>
-    <div className="text-xl font-medium text-black"> <div onClick={() => props.whenNoteClicked(props.id)}>
-              <h3>{props.title}</h3>
-              </div>
-              </div>
-              <p className="text-slate-500">Date</p>
-  </div>
-</div>
-</div>
+      <div className="container">
+      <Card className="note-item">
+        <div className="note-item__title">
+          <div onClick={() => props.whenNoteClicked(props.id)}>
+            <h3>{props.title}</h3>
+          </div>
+        </div>
 
+        <p>Date</p>
+      </Card>
+      </div>
     </React.Fragment>
   );
 }
@@ -35,6 +30,24 @@ Note.propTypes = {
 
 export default Note;
 
-
 // -----NOTES-----
 // WIP: work on fixing grid layout
+
+// function Note(props) {
+//   return (
+//     <React.Fragment>
+// <div className="mt-12 h-20 flex flex-wrap justify-center">
+// <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+//   <div>
+//     <div className="text-xl font-medium text-black"> <div onClick={() => props.whenNoteClicked(props.id)}>
+//               <h3>{props.title}</h3>
+//               </div>
+//               </div>
+//               <p className="text-slate-500">Date</p>
+//   </div>
+// </div>
+// </div>
+
+//     </React.Fragment>
+//   );
+// }
