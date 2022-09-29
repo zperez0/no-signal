6be5023c94c3ba743c8401ea339ec057a1af8b1d
Yazmin Perez <zoeperez333@gmail.com>
2022-09-29 11:13:59 -0700

    style sign in/out forms. add card

--- src/components/users/SignUp.jsx
+++ src/components/users/SignUp.jsx
@@ -24,38 +24,56 @@ const SignUp = () => {
   };
 
   return (
-    <div className="max-w-[700px] mx-auto my-16 p-4">
-      <div>
-        <h1 className="text-2xl font-bold py-2">Sign up for a free account</h1>
-        <p className="py-2">
-          Already have an account yet?{" "}
-          <Link to="/" className="underline">
-            Sign in.
-          </Link>
-        </p>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Email Address</label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            className="border p-3"
-            type="email"
-          />
-        </div>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Password</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            className="border p-3"
-            type="password"
-          />
-        </div>
-        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
-          Sign Up
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="container py-12 px-6 h-full">
+        <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+              <div className="max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
+                <div className="signCard">
+                  <div className="h-100 w-full object-cover md:h-full md:w-80 ">
+                    <div>
+                      <h1 className="text-2xl font-bold py-2 text-center">
+                        Sign up
+                      </h1>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                      <div className="boarder white flex flex-col py-2">
+                        <label className="py-2 font-medium ">
+                          Email Address
+                        </label>
+                        <input
+                          onChange={(e) => setEmail(e.target.value)}
+                          className="border p-3 rounded"
+                          type="email"
+                        />
+                      </div>
+                      <div className="flex flex-col py-2">
+                        <label className="py-2 font-medium">Password</label>
+                        <input
+                          onChange={(e) => setPassword(e.target.value)}
+                          className="border p-3 rounded"
+                          type="password"
+                        />
+                      </div>
+
+                      <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white font-bold rounded">
+                        Sign up
+                      </button>
+                      <p className="inline-block p-2">
+                        Already have an account?{" "}
+                        <Link
+                          to="/"
+                          className="text-yellow-500  hover:underline"
+                        >
+                          Sign in.
+                        </Link>
+                      </p>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    </>
   );
 };
 
