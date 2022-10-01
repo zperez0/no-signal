343e2304c1d31eb090eb6822a76c8183540fd82a
Yazmin Perez <zoeperez333@gmail.com>
2022-09-30 18:23:40 -0700

    add userid to note

--- src/components/notes/NewNoteForm.js
+++ src/components/notes/NewNoteForm.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
 
+import { UserAuth } from "../../context/AuthContext";
+
 function NewNoteForm(props) {
+  
+  const {user} = UserAuth();
 
   function handleNewNoteFormSubmission(event) {
     event.preventDefault()
     props.onNewNoteCreation({
       title: event.target.title.value,
-      body: event.target.body.value
+      body: event.target.body.value,
+      username: user.displayName,
+      userId: user.uid,
     })
   }
   return (
