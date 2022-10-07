ecbafe07dcf66537363b368262e2642e5bdbe6ac
Yazmin Perez <zoeperez333@gmail.com>
2022-10-07 12:41:52 -0700

    hide sidebar if not signed in

--- src/components/navbar/SideBar.js
+++ src/components/navbar/SideBar.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { BsGearFill } from 'react-icons/bs';
-import { FaCompass } from 'react-icons/fa';
+import { FaCompass, FaUserCircle } from 'react-icons/fa';
 import { IoMapOutline } from "react-icons/io5";
 import { GiBackpack } from "react-icons/gi";
 import { useNavigate } from "react-router-dom";
 
+import { UserAuth } from "../../context/AuthContext";
+
+// to commit
+  //changed trails route to parks
+  // changed gear icon to user circle
 
 const SideBar = ({ text }) => {
   const navigate = useNavigate();
-
+  const {user} = UserAuth();
+  
+if(user.uid) { 
   const handleTrails = () => {
-    navigate("/trails");
+    navigate("/parks");
   }
 
   const handleMap = () => {
@@ -41,11 +47,12 @@ const SideBar = ({ text }) => {
         </button>
 
         <button onClick={handleAccount}>
-        <SideBarIcon icon={<BsGearFill size="22" />} />
+        <SideBarIcon icon={<FaUserCircle size="24" />} />
         </button>
     </div>
   );
 };
+}
 const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
   <div className="sidebar-icon group">
     {icon}
@@ -56,3 +63,58 @@ const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
 );
 
 export default SideBar
+
+
+
+// ----------------------------------------
+
+// const SideBar = ({ text }) => {
+//   const navigate = useNavigate();
+
+//   const handleTrails = () => {
+//     navigate("/parks");
+//   }
+
+//   const handleMap = () => {
+//     navigate("/map");
+//   }
+
+//   const handleNotes = () => {
+//     navigate("/notes");
+//   }
+
+//   const handleAccount = () => {
+//     navigate("/account");
+//   }
+
+//   return (
+//     <div className="fixed top-0 left-0 h-screen w-20 flex flex-col
+//                   bg-white dark:bg-gray-900 shadow-lg">
+//                 <button onClick={handleTrails}>   
+//         <SideBarIcon icon={<FaCompass size="24" />} />
+//         </button> 
+        
+//         <button onClick={handleMap}>   
+//         <SideBarIcon icon={<IoMapOutline size="29" />} />
+//         </button>
+
+//         <button onClick={handleNotes}>
+//         <SideBarIcon icon={<GiBackpack size="33" />} />
+//         </button>
+
+//         <button onClick={handleAccount}>
+//         <SideBarIcon icon={<FaUserCircle size="24" />} />
+//         </button>
+//     </div>
+//   );
+// };
+// const SideBarIcon = ({ icon, text = 'tooltip 💡' }) => (
+//   <div className="sidebar-icon group">
+//     {icon}
+//     <span className="sidebar-tooltip group-hover:scale-100">
+//     {text}
+//     </span>
+//   </div>
+// );
+
+// export default SideBar
