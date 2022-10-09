ae590938ad7b8187c75dc5b69c5c0982b30ca0e9
Yazmin Perez <zoeperez333@gmail.com>
2022-10-09 12:15:50 -0700

    add card to profile

--- src/components/users/Account.jsx
+++ src/components/users/Account.jsx
@@ -20,13 +20,20 @@ const Account = () => {
   return (
     <>
       <div className="background">
-        <div className="max-w-[600px] mx-auto  p-4">
-          <h1 className="text-2xl font-bold py-4">Account</h1>
-          <p>Username: {user && user.displayName}</p>
-          <p>User Email: {user && user.email}</p>
-          <button onClick={handleLogout} className="border px-6 py-2 my-4">
-            Logout
-          </button>
+        <div className=" container py-10">
+          <div class="block p-24 rounded-lg shadow-lg bg-white opacity-75 max-w-sm py-2">
+            <div className="max-w-[600px] mx-auto  p-4">
+              <h1 className="text-2xl font-bold py-4">Account</h1>
+              <p>{user && user.displayName}</p>
+              <p className="pb-4">{user && user.email}</p>
+              <button
+                onClick={handleLogout}
+                className="px-4 py-2 text-sm text-black-600 font-semibold rounded-full border hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2"
+              >
+                Logout
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
